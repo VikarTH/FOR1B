ceb0f0a984b3948b643529fd4e9c28cb6005512b
VikarTH <vikarth276@nemi.tskoli.is>
2017-01-25 10:47:24 +0000

    Dæmi 2

---
+++ daemi_2.py
@@ -0,0 +1,10 @@
+#Dæmi 2
+#Búið til forrit gerir notananda kleyft að slá inn fornafn og eftirnafn. Forritið heilsar síðan notanda
+#með fornafni og eftirnafni.
+print("Sláðu inn fornafn")
+fornafn = input("=")
+print("Sláðu inn eftirnafn")
+eftirnafn = input("=")
+
+#Forritið heilsar uppá notanda
+print("Góðan daginn", fornafn, " ", eftirnafn)
